b23b0574f3765285555e87482a5223acbbfae355
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-02-19 19:05:47 -0500

    Create FixDebugTwo3.java

---
+++ FixDebugTwo3.java
@@ -0,0 +1,9 @@
+public class FixDebugTwo3 {
+    public static void main(String[] args) {
+        int a = 5, b = 10, sum;
+        
+        sum = a + b;
+        
+        System.out.println("The sum is " + sum);
+    }
+}
