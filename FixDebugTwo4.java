e3ac9ee8b5c3c2bbd6ee43876657c4b034a52e8d
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-02-19 19:06:22 -0500

    Create FixDebugTwo4.java

---
+++ FixDebugTwo4.java
@@ -0,0 +1,10 @@
+public class FixDebugTwo4 {
+    public static void main(String[] args) {
+        int num = 2;
+        
+        for (int i = 0; i < 5; i++) {
+            num *= 2;
+            System.out.println("Value of num: " + num);
+        }
+    }
+}
